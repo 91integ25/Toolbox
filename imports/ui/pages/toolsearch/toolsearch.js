e0b84dbea92d194a52dc46f71479467f6de9e89e
91integ25 <91integ25@gmail.com>
2017-03-24 19:15:28 -0400

    fix issues

--- imports/ui/pages/toolsearch/toolsearch.js
+++ imports/ui/pages/toolsearch/toolsearch.js
@@ -1,17 +1,4 @@
 import './toolsearch.html';
 import { Accounts } from 'meteor/accounts-base';
-import { toolrent } from '../toolrent/toolrent.js';
+import { test } from '../toolrent/toolrent.js';
 
-Template.toolsearch.events({
-  'click .toolsearch-btn': function (e) {
-    e.preventDefault();
-    console.log(`from register: ${e}`);
-
-      var locate = test.find().fetch();
-      var tool = $(".tool").val().text();
-      console.log(locate);
-      
-      for(i=0; i<locate.length; i++)
-
-  	},
-})
